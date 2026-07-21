c437a2d4a7052c98a3d50e379cde4edd5bad7dbe
Gaccio <164079244+StoGaccio@users.noreply.github.com>
2026-07-21 18:42:48 +0200

    Aggiunte parti di codice e aggiunta commenti
    Ho aggiunto parti di codice funzionali alla stampa di alcuni elementi nella lista di cose da portare.
    Ho aggiunto i commenti al codice per capire cosa fa cosa

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
-import streamlit as st
-from plurali import plul_calz, plul_mut, plul_pant, plurale_magliett, plul_fel
+#importo le librerie
+import streamlit as st  #streamlit
+from plurali import plul_calz, plul_mut, plul_pant, plurale_magliett, plul_fel #funzioni per il plurale
 
 if __name__ == '__main__':
-    g = st.number_input('Inserisci il numero di giorni di viaggio:', min_value=1, step=1)
-    cf = g*2
-    if g > 7:
+    
+#ottenimento dati
+    g = st.number_input('Inserisci il numero di giorni di viaggio:', min_value=1, step=1)   #numero giorni
+
+    cf = g*2   #numero di cotton fioc
+
+    if g > 7:   #presenza di lavatrice
         vl = st.radio('Avrai a disposizione una lavatrice?', ('Y', 'N'))
         if vl.lower() == 'y':
             g = 7
-    ma = st.radio('In questa vacanza avrai bisogno di un costume?', ('Y', 'N'))
+
+    ma = st.radio('In questa vacanza avrai bisogno di un costume?', ('Y', 'N')) #costume
     co = ma.lower() == 'y'
-    m = g + 1
-    p = g//2
+
+    m = g + 1   #numero magliette
+
+    p = g//2    #numero pantaloni
     if p < 1:
         p = 1
-    f = p-1
+    
+    f = p-1 #numero felpe
     if f < 1:
         f = 1
-    mu = g + 1
-    c = g + 1
-    vcs = st.radio('Crema solare?', ('Y', 'N'))
-    cs = vcs.lower() == 'y'
+    
+    mu = g + 1  #numero mutande
 
+    c = g + 1   #numero calze
+
+    vcs = st.radio('Crema solare?', ('Y', 'N')) #crema solare
+    cs = False
+    if vcs.lower() == 'y':
+        cs = True
+
+
+#stampa lista
     st.write('Eccoti la lista delle cose da mettere in valigia:\n')
     st.write('- ' + str(m) + ' ' + plurale_magliett(m)) #magliette
     st.write('- ' + str(p) + ' ' + plul_pant(p)) #pantaloni
@@ -29,4 +45,16 @@ if __name__ == '__main__':
     st.write('- ' + str(mu) + ' ' + plul_mut(mu)) #mutande
     st.write('- ' + str(f) + ' ' + plul_fel(f)) #felpe
     st.write('- Beauty case:\n   - Spazzolino\n   - Dentifricio\n   - Shampoo\n   - Bagnoschiuma\n   - '+str(cf)+' Cotton fioc\n   - Deodorante\n   - Pettine\n   - Lametta\n   - Saponetta\n   - Polvere per capelli\n   - Schiuma da barba')
-    st.write('\nBUON VIAGGIO!')
+    #beauty case
+    st.write('- Caricabatterie\n   - Trasformatore\n  - Cavi per i tuoi dispositivi, non li so')    #cavetteria
+    st.write('- Cuffie')    #cuffie e cuffiette
+    st.write('- Cuffiette')
+    st.write('- Powerbank') #powerbank
+    st.write('- Portafoglio')   #portafoglio
+    st.write('- Libri/Kindle')  #roba da leggere
+    st.write('- K-way')   #kway
+    st.write('- Pigiama')   #pigiama
+    if cs == True:  #crema solare
+        st.write('- Crema solare')
+
+    st.write('\nBUON VIAGGIO!') #saluti
